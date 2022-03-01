72ff83a00d00df34ca1ba79205a9f665cb482d21
Marco Catanzariti <marcocatanzariti@gmail.com>
2022-03-01 14:31:03 +1100

    second commit

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,7 @@
-import { myPhrase } from "./secret";
+import { myPhrase, deployerAddress, deployerAddress } from "./secret";
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const mnemonic = myPhrase;
+const mnemonic = myPhrase; 
+const deployerAddress = deployerAddress;
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -67,7 +68,7 @@ module.exports = {
     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     skipDryRun: true,    // Skip dry run before migrations? (default: false for public nets )
-    from: "0x5aE4755B3CE84E27c7c3E4c027c8155935b20A66"
+    from: deployerAddress
     },
    // Useful for private networks
     // private: {
